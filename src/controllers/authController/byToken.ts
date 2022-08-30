accc62c9fb6541b372e05611644856ded05115b3
KalyDanil <94454437+KalyDanil@users.noreply.github.com>
2022-08-30 11:38:13 +0300

    Delete src/controllers/authController directory

--- src/controllers/authController/byToken.ts
+++
@@ -1,9 +0,0 @@
-import type { Response, NextFunction } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import type { IReqUser } from '../../types/req';
-
-export const authorizationByToken = async (req: IReqUser, res: Response, next: NextFunction) => {
-  try {
-    return res.status(StatusCodes.OK).json(req.user);
-  } catch (err) { next(err); }
-};
